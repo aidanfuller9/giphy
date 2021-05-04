6041d31d641a5bdd8f413f9561510a7e42e57ebb
Glitch (aidan-giphy) <none>
2021-05-04 00:57:41 +0000

    🚩🕹 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
+let custom_api_url = ""
 $(".search-button").click(function(){
-  
-
+  fetch(custom_api_url)
+  .then(function(response) {
+        return response.json();
+        })
+  .then(function(data) {
+        console.log(data);
+        console.log(data.custom_api_url);
+        });
+console.log(custom_api_url)
   
 });
 
